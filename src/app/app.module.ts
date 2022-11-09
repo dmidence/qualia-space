568aaaa0431b6352e50fb696d0eb50348ed292e9
dmidence <40371373+dmidence@users.noreply.github.com>
2022-11-08 23:02:16 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LandingNavbarComponent } from './components/shared/landing-navbar/landing-navbar.component';
+import { LandingFooterComponent } from './components/shared/landing-footer/landing-footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LandingComponent, LandingNavbarComponent, LandingFooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
