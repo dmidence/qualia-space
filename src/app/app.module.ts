1911d477ce9c56628e4e39129a272487d7141154
dmidence <40371373+dmidence@users.noreply.github.com>
2022-11-13 14:22:46 -0600

    register, register enterprise, login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { LandingNavbarComponent } from './components/shared/landing-navbar/landing-navbar.component';
 import { LandingFooterComponent } from './components/shared/landing-footer/landing-footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterUserComponent } from './pages/register-user/register-user.component';
+import { RegisterEnterpriseComponent } from './pages/register-enterprise/register-enterprise.component';
+import { HomeuserComponent } from './pages/homeuser/homeuser.component';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, LandingNavbarComponent, LandingFooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    LandingNavbarComponent,
+    LandingFooterComponent,
+    LoginComponent,
+    RegisterUserComponent,
+    RegisterEnterpriseComponent,
+    HomeuserComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
